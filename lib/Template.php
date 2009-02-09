e22c057bc1be7a8eb56a7142abbec0a11334c49e
Pete Deffendol <pete@mdsc.com>
2009-02-09 09:57:58 -0700

    Created Template, with variable assignment functionality

---
+++ lib/Template.php
@@ -0,0 +1,48 @@
+<?php
+class Tingle_Template
+{
+	protected $_config;
+	
+	public function __construct($config = null)
+	{
+
+	}
+	
+	public function assign($name_or_container, $value = null)
+	{
+		if (is_string($name_or_container))
+		{
+			if ($name_or_container != '_config')
+			{
+				$this->$name_or_container = $value;
+				return true;
+			}
+		}
+		
+		if (is_array($name_or_container))
+		{
+			// Assign key/value pairs
+			foreach ($name_or_container as $key => $value)
+			{
+				$this->assign($key, $value);
+			}
+			return true;
+		}
+
+		if (is_object($name_or_container))
+		{
+			return $this->assign(get_object_vars($name_or_container));
+		}
+		
+		// Name isn't a string or container
+		return false;
+	}
+	
+	public function get_assignments()
+	{
+		$all = (array)get_object_vars($this);
+		unset($all['_config']);
+		return $all;
+	}
+}
+?>
---
+++ test/AssignmentTest.php
@@ -0,0 +1,56 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+require_once dirname(__FILE__).'/../lib/Template.php';
+
+class AssignmentTest extends PHPUnit_Framework_TestCase
+{
+	protected function setUp()
+	{
+		$this->tpl = new Tingle_Template;	
+	}
+	
+	public function test_should_start_with_no_assignments()
+  {
+		$this->assertTrue(is_array($this->tpl->get_assignments()));
+		$this->assertEquals(0, count($this->tpl->get_assignments()));
+	}
+	
+	public function test_should_assign_single_value()
+	{
+		$this->tpl->assign('test_data', 'Hello');
+		
+		$this->assertEquals('Hello', $this->tpl->test_data);
+		$this->assertEquals(1, count($this->tpl->get_assignments()));
+	}
+	
+	public function test_should_assign_hash()
+	{
+		$data = array('one' => 'Hello', 'two' => 'World');
+		$this->tpl->assign($data);
+		
+		$this->assertEquals(2, count($this->tpl->get_assignments()));
+		$this->assertEquals('Hello', $this->tpl->one);
+		$this->assertEquals('World', $this->tpl->two);
+	}
+	
+	public function test_should_assign_object_properties()
+	{
+		$obj = new StdClass;
+		$obj->one = 'Hello';
+		$obj->two = 'World';
+		$this->tpl->assign($obj);
+		
+		$this->assertEquals(2, count($this->tpl->get_assignments()));
+		$this->assertEquals('Hello', $this->tpl->one);
+		$this->assertEquals('World', $this->tpl->two);
+	}
+
+	public function test_should_allow_direct_assignment()
+	{
+		$this->tpl->test_data = 'Hello';
+	
+		$this->assertEquals('Hello', $this->tpl->test_data);
+		$this->assertEquals(1, count($this->tpl->get_assignments()));
+	}
+}
+?>
