1276729a306a82206b37ef416a9361196df2156a
Pete Deffendol <pete@mdsc.com>
2009-02-09 11:59:47 -0700

    Implemented basic template rendering

---
+++ lib/Exception.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* 
+*/
+class Tingle_Exception extends Exception
+{
+}
+
+class Tingle_TemplateNotFoundException extends Tingle_Exception
+{
+}
+
+?>
--- lib/Template.php
+++ lib/Template.php
@@ -1,13 +1,52 @@
 <?php
+require_once dirname(__FILE__).'/Exception.php';
+
 class Tingle_Template
 {
-	protected $_config;
+	protected $_config = array(
+		'template_path' => array('.'),
+		'template'      => null
+		);
+
 	
 	public function __construct($config = null)
 	{
 
 	}
+
 	
+	/**
+	 * Assign variables to the template.
+	 *
+	 * Variables may be assigned one at a time, or you can assign
+	 * all the properties of an object or associated array.
+	 *
+	 * Assigning a variable named _config is not allowed, as it would
+	 * interfere with the Template settings.
+	 *
+	 * Examples:
+	 * 
+	 * <code>
+	 * $tpl = new Tingle_Template;
+	 *
+	 * // Assign single variable
+   * $tpl->assign('var', 'value');
+	 *
+	 * // Assign associative array
+	 * $data = array('var1' => 'value1', 'var2' => 'value2);
+	 * $tpl->assign($data);
+	 *
+	 * // Assign object properties
+	 * $obj = new StdClass;
+	 * $obj->var1 = 'value1';
+	 * $obj->var2 = 'value2';
+	 * $tpl->assign($obj);
+	 *
+	 * // Assign directory to template object
+	 * $tpl->var = 'value'
+	 *
+	 * @return boolean True on success, false on failure
+	 */
 	public function assign($name_or_container, $value = null)
 	{
 		if (is_string($name_or_container))
@@ -38,7 +77,13 @@ class Tingle_Template
 		// Name isn't a string or container
 		return false;
 	}
+
 	
+	/**
+	 * Return all assigned variables, as an associative array.
+	 *
+	 * @return array Assigned template variables
+	 */
 	public function get_assignments()
 	{
 		$all = (array)get_object_vars($this);
@@ -48,6 +93,135 @@ class Tingle_Template
 		unset($all['_config']);
 		
 		return $all;
+	}
+	
+	/**
+	 * Configuration
+	 */
+	
+	/**
+	 * Add another directory to the template search path.
+	 *
+	 * @param string $path Path to location of templates in filesystem
+	 */
+	public function add_template_path($path)
+	{
+		$this->_config['template_path'][] = $path;
+	}
+
+	
+	/**
+	 * Set the template search path, overwriting any previous settings.
+	 *
+	 * @param string $path New template search path
+	 */
+	public function set_template_path($path)
+	{
+		$this->_config['template_path'] = (array)$path;
+	}
+
+	/**
+	 * Template rendering
+	 */
+
+	
+	/**
+	 * Indicate template file to process when calling ::fetch() and ::render() without
+	 * parameters.
+	 *
+	 * @param string $template Path to template file
+	 */
+	public function set_template($template)
+	{
+		$this->_config['template'] = $template;
+	}
+
+	
+	/**
+	 * Convert a Template object to a string by obtaining the rendered template.
+	 * If a template has not been set with ::set_template() then return an
+	 * empty string.
+	 *
+	 * @return string Rendered template
+	 **/
+	public function __toString()
+	{
+		if ($this->_config['template'])
+		{
+			return $this->fetch();
+		}
+		
+		return '';
+	}
+
+	
+	/**
+	 * Process a template file and output the results.  If a template filename
+	 * is not provided, then use the filename already provided by ::set_template()
+	 *
+	 * @param string $template Path to template file
+	 */
+	public function render($template = null)
+	{
+		echo $this->fetch($template);
+	}
+	
+	
+	/**
+	 * Process a template file and return the results as a string.  If a template 
+	 * filename is not provided, then use the filename already provided by 
+	 * ::set_template()
+	 *
+	 * @param string $template Path to template file
+	 * @return string Results of processing template
+	 */
+	public function fetch($template = null)
+	{
+		if ($template === null)
+		{
+			$template = $this->_config['template'];
+		}
+		
+		if (false === ($template_path = $this->get_template_path($template)))
+		{
+			throw new Tingle_TemplateNotFoundException('Template '.$template.' not found');
+		}
+		
+		// Capture template output
+		ob_start();
+		include($template_path);
+		return ob_get_clean();
+	}
+
+	
+	/**
+	 * 	Attempt to locate the specified template in the template
+	 * path and return its full path.
+	 *
+	 * @param  string Name of desired template file
+	 * @return mixed Full path to template, or false if not found
+	 */
+	private function get_template_path($template)
+	{
+		$paths = (array)$this->_config['template_path'];
+		
+		foreach ($paths as $path)
+		{
+			$full_path = realpath($path.DIRECTORY_SEPARATOR.$template);
+			$real_path = realpath($path);
+			
+			// Only templates inside the template_path directories are
+			// allowed to be executed
+			if (file_exists($full_path) 
+				&& is_readable($full_path) 
+				&& substr($full_path, 0, strlen($real_path)) == $real_path)
+			{
+				return $full_path;
+			}
+		}
+		
+		// File not found in any path
+		return false;
 	}
 }
 ?>
