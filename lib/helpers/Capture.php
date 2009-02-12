cf9ed229690a551de734ce06f326a00038abe338
Pete Deffendol <pete@mdsc.com>
2009-02-12 12:18:42 -0700

    Added some docs for content_for helper

--- lib/helpers/Capture.php
+++ lib/helpers/Capture.php
@@ -12,6 +12,28 @@ class Tingle_Helper_Capture extends Tingle_Helper
 		$this->contents = array();
 	}
 	
+	
+	/**
+	 * Access and manipulate a piece of captured content.
+	 *
+	 * This is useful for creating placeholders in your templates,
+	 * which can then be filled with content extracted from another part
+	 * of the template.
+	 *
+	 * Basic Usage
+	 *
+	 * <code>
+	 * <?php $this->capture_for('sidebar')->start(); ?>
+	 * Sidebar content goes here
+	 * <?php $this->capture_for('sidebar')->end(); ?>
+	 *
+	 * <div id="content">Main content</div>
+	 * <div id="sidebar"><?php echo $this->capture_for('sidebar'); ?></div>
+	 * </code>
+	 *
+	 * @param string $name Name of content
+	 * @return object New or existing Tingle_Helper_Capture_Content object
+	 */
 	public function content_for($name)
 	{
 		if (!isset($this->contents[$name]))
