f142b44c04535fcf7483e8165630758eca050a06
Pete Deffendol <pete@mdsc.com>
2009-03-31 10:41:54 -0600

    Make form helper tests a little more decisive, and add (broken) test for select helper

--- test/FormHelperTest.php
+++ test/FormHelperTest.php
@@ -4,49 +4,54 @@ require_once dirname(__FILE__).'/../lib/helpers/FormHelper.php';
 
 class MockBuilder extends Tingle_FormBuilder
 {
-	public function checkbox($name, $attributes = array(), $checked_value = '1', $unchecked_value = '0')
+	public function checkbox($name, $html_attributes = array(), $checked_value = '1', $unchecked_value = '0')
 	{
-		return 'mock form tag';
+		return 'checkbox';
 	}
 	
-	public function file_field($name, $attributes = array())
+	public function file_field($name, $html_attributes = array())
 	{
-		return 'mock form tag';
+		return 'file field';
 	}
 	
-	public function grouped_checkbox($name, $tag_value, $attributes = array())
+	public function grouped_checkbox($name, $tag_value, $html_attributes = array())
 	{
-		return 'mock form tag';
+		return 'grouped checkbox';
 	}
 
-	public function hidden_field($name, $attributes = array())
+	public function hidden_field($name, $html_attributes = array())
 	{
-		return 'mock form tag';
+		return 'hidden field';
 	}
 
-	public function label($name, $text, $attributes = array())
+	public function label($name, $text, $html_attributes = array())
 	{
-		return 'mock form tag';
+		return 'label';
 	}
 
-	public function password_field($name, $attributes = array())
+	public function password_field($name, $html_attributes = array())
 	{
-		return 'mock form tag';
+		return 'password field';
 	}
 
-	public function radio_button($name, $value, $attributes = array())
+	public function radio_button($name, $value, $html_attributes = array())
 	{
-		return 'mock form tag';
+		return 'radio button';
+	}
+	
+	public function select($name, $value, $options = array(), $html_attributes = array())
+	{
+		return 'select';
 	}
 
-	public function text_area($name, $attributes = array())
+	public function text_area($name, $html_attributes = array())
 	{
-		return 'mock form tag';
+		return 'text area';
 	}
 
-	public function text_field($name, $attributes = array())
+	public function text_field($name, $html_attributes = array())
 	{
-		return 'mock form tag';
+		return 'text field';
 	}
 }
 
@@ -90,49 +95,55 @@ class FormHelperTest extends PHPUnit_Framework_TestCase
 	public function test_checkbox()
 	{
 		Tingle_FormHelper::start_form_for(array(), array('builder' => 'MockBuilder'));
-		$this->assertEquals('mock form tag', Tingle_FormHelper::checkbox('foo'));
+		$this->assertEquals('checkbox', Tingle_FormHelper::checkbox('foo'));
 	}
 	
 	public function test_file_field()
 	{
 		Tingle_FormHelper::start_form_for(array(), array('builder' => 'MockBuilder'));
-		$this->assertEquals('mock form tag', Tingle_FormHelper::file_field('foo'));
+		$this->assertEquals('file field', Tingle_FormHelper::file_field('foo'));
 	}
 	
 	public function hidden_field()
 	{
 		Tingle_FormHelper::start_form_for(array(), array('builder' => 'MockBuilder'));
-		$this->assertEquals('mock form tag', Tingle_FormHelper::hidden_field('foo'));
+		$this->assertEquals('hidden field', Tingle_FormHelper::hidden_field('foo'));
 	}
 	
 	public function test_label()
 	{
 		Tingle_FormHelper::start_form_for(array(), array('builder' => 'MockBuilder'));
-		$this->assertEquals('mock form tag', Tingle_FormHelper::label('foo'));
+		$this->assertEquals('label', Tingle_FormHelper::label('foo'));
 	}
 	
 	public function test_password_field()
 	{
 		Tingle_FormHelper::start_form_for(array(), array('builder' => 'MockBuilder'));
-		$this->assertEquals('mock form tag', Tingle_FormHelper::password_field('foo'));
+		$this->assertEquals('password field', Tingle_FormHelper::password_field('foo'));
 	}
 	
 	public function test_radio_button()
 	{
 		Tingle_FormHelper::start_form_for(array(), array('builder' => 'MockBuilder'));
-		$this->assertEquals('mock form tag', Tingle_FormHelper::radio_button('foo', 'bar'));
+		$this->assertEquals('radio button', Tingle_FormHelper::radio_button('foo', 'bar'));
+	}
+	
+	public function test_select()
+	{
+		Tingle_FormHelper::start_form_for(array(), array('builder' => 'MockBuilder'));
+		$this->assertEquals('select', Tingle_FormHelper::select('foo', 'bar'));
 	}
 	
 	public function test_text_area()
 	{
 		Tingle_FormHelper::start_form_for(array(), array('builder' => 'MockBuilder'));
-		$this->assertEquals('mock form tag', Tingle_FormHelper::checkbox('foo'));
+		$this->assertEquals('text area', Tingle_FormHelper::text_area('foo'));
 	}
 	
 	public function test_text_field()
 	{
 		Tingle_FormHelper::start_form_for(array(), array('builder' => 'MockBuilder'));
-		$this->assertEquals('mock form tag', Tingle_FormHelper::checkbox('foo'));
+		$this->assertEquals('text field', Tingle_FormHelper::text_field('foo'));
 	}
 }
 ?>
