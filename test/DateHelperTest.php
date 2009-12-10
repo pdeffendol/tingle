d7f17615223892685b7dbae29f4942602db19ea3
Pete Deffendol <pete@mdsc.com>
2009-12-10 13:49:45 -0700

    Added a datetime_tag helper that implements the Datetime design pattern for HTML.

---
+++ test/DateHelperTest.php
@@ -0,0 +1,65 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+require_once dirname(__FILE__).'/../lib/helpers/DateHelper.php';
+
+class DateHelperTest extends PHPUnit_Framework_TestCase
+{
+	private $date = 'Mon, 15 Aug 05 15:52:01 +0000';
+	
+	public function test_date_with_default_format()
+	{
+		$this->assertEquals('Mon, 15 Aug 05 15:52:01 +0000', Tingle_DateHelper::format_date(new DateTime($this->date)));
+	}
+	
+	public function test_date_with_format()
+	{
+		$this->assertEquals('08/15/2005', Tingle_DateHelper::format_date(new DateTime($this->date), 'm/d/Y'));
+	}
+	
+	public function test_date_string()
+	{
+		$this->assertEquals('Mon, 15 Aug 05 15:52:01 +0000', Tingle_DateHelper::format_date($this->date));
+	}
+	
+	public function test_datetime_tag_defaults()
+	{
+		$actual = Tingle_DateHelper::datetime_tag($this->date);
+		$matcher = array('tag' => 'abbr',
+		                 'attributes' => array(
+			                 'title' => '2005-08-15T15:52:01+0000'),
+									 	 'content' => 'Mon, 15 Aug 05 15:52:01 +0000');
+		$this->assertTag($matcher, $actual, 'Default attributes');
+	}
+	
+	public function test_datetime_tag_with_format()
+	{
+		$actual = Tingle_DateHelper::datetime_tag($this->date, 'm/d/Y');
+		$matcher = array('tag' => 'abbr',
+		                 'attributes' => array(
+			                 'title' => '2005-08-15T15:52:01+0000'),
+										 'content' => '08/15/2005');
+		$this->assertTag($matcher, $actual, 'format specified');
+	}
+	
+	public function test_datetime_tag_with_html_attributes()
+	{
+		$actual = Tingle_DateHelper::datetime_tag($this->date, 'm/d/Y', array('class' => 'date'));
+		$matcher = array('tag' => 'abbr',
+		                 'attributes' => array(
+										   'class' => 'date',
+			                 'title' => '2005-08-15T15:52:01+0000'),
+										 'content' => '08/15/2005');
+		$this->assertTag($matcher, $actual, 'additional attributes specified');
+	}
+	
+	public function test_datetime_tag_overwrites_specified_title_attribute()
+	{
+		$actual = Tingle_DateHelper::datetime_tag($this->date, 'm/d/Y', array('title' => 'expendable'));
+		$matcher = array('tag' => 'abbr',
+		                 'attributes' => array(
+			                 'title' => '2005-08-15T15:52:01+0000'),
+										 'content' => '08/15/2005');
+		$this->assertTag($matcher, $actual, 'title attribute specified');
+	}
+}
+?>
